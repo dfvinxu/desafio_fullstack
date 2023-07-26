0ac01967c00fea5f54c0cce91fdf9c32128cac60
aerozfx <aerozfx@gmail.com>
2023-07-26 17:35:41 +0200

    rebuild vite project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,7 @@
-import React from "react";
-import { CssBaseline, Grid } from "@material-ui/core";
+import './App.css'
+import "../styles/styles.scss"
+function App() {
 
-import Header from "./components/Header/Header";
-import List from "./components/List/List";
-import Map from "./components/Map/Map";
+}
 
-const App = () => {
-  return (
-    <>
-      <CssBaseline />
-      <Header />
-      <Grid container spacing={3} style={{ width: "100%" }}>
-        <Grid item xs={12} md={4}>
-          <List />
-        </Grid>
-        <Grid item xs={12} md={8}>
-          <Map />
-        </Grid>
-      </Grid>
-    </>
-  );
-};
-
-export default App;
+export default App
