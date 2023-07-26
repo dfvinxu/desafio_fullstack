a82b357f024f0e9ef882bb1992039f9fd4922f91
Rebeca <becabecks3.gmail.com>
2023-07-26 17:49:41 +0200

    add/token-cookie

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,23 +1,57 @@
 const User = require('../models/users-sequelize');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const jwtSecret = process.env.JWT_SECRET;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// SIGNUP
+const signUpUser = async (req, res, next) => {
+    try {
+      const { user, name, surname, email, password, nationality, birth_date } = req.body;
+      const hashPassword = await bcrypt.hash(password, saltRounds);
+      const newUser = {
+        user,
+        name,
+        surname,
+        email,
+        password: hashPassword,
+        nationality,
+        birth_date,
+      };
+      const createdUser = await User.create(newUser);
+      req.user = { email: createdUser.email };
+      next();
+    } catch (error) {
+      console.log('Error:', error);
+      res.status(500).json({ "msj": "Error en el registro" });
+    }
+};
 
 
+
+//LOGIN
 const checkEmailLogIn = async (req, res, next) => {
     let { email, password } = req.body;
     try {
         // Buscar el usuario por correo electrónico en la base de datos
         let user = await User.findOne({ where: { email } });
-
         if (!user) {
             console.log("This email does not have an account");
             return res.status(401).json({ "msj": "This email does not have an account" });
         }
-
         // Verificar si la contraseña coincide
-        if (user.password !== password) {
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
             console.log("Incorrect password");
             return res.status(401).json({ "msj": "Incorrect password" });
         }else {
-            res.status(200).json({"user": user });
+            const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: '300h' }); 
+            res.cookie("access-token", token, {
+                httpOnly: true,
+                sameSite: "lax",
+            })
+            res.status(200).json({ "user": user, "token": token });
         }    
 
     } catch (err) {
@@ -27,6 +61,15 @@ const checkEmailLogIn = async (req, res, next) => {
 };
 
 
+
+//LOG OUT
+const logOut = (req, res, next) => {
+    res.clearCookie("access-token"); 
+    res.redirect('/login'); 
+}
+
 module.exports = {
-    checkEmailLogIn
+    checkEmailLogIn,
+    signUpUser,
+    logOut
 } ;
--- server/routes/auth_routes.js
+++ server/routes/auth_routes.js
@@ -7,7 +7,7 @@ const authController = require('../controllers/auth')
 authRouter.post('/auth/login', authController.checkEmailLogIn)
 
 // //SIGN UP
-// authRouter.post('/auth/signup', authMiddleware.signUpUser, authController.createAndStoreToken)
+authRouter.post('/auth/signup', authController.signUpUser)
 
 // // Log out
 // authRouter.get('/logout', authMiddleware.authCheck, authController.destroySessionAndClearCookies);
