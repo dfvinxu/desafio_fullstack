8be2039bad12c72baed32b668baecc52aa738c72
aerozfx <aerozfx@gmail.com>
2023-07-26 12:26:37 +0200

    pull from remote repository

---
+++ server/controllers/auth.js
@@ -0,0 +1,31 @@
+const User = require("../models/users-sequelize");
+
+const checkEmailLogIn = async (req, res, next) => {
+  let { email, password } = req.body;
+  try {
+    // Buscar el usuario por correo electrónico en la base de datos
+    let user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      console.log("This email does not have an account");
+      return res
+        .status(401)
+        .json({ msj: "This email does not have an account" });
+    }
+
+    // Verificar si la contraseña coincide
+    if (user.password !== password) {
+      console.log("Incorrect password");
+      return res.status(401).json({ msj: "Incorrect password" });
+    } else {
+      res.status(200).json({ user: user });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msj: " Error" });
+  }
+};
+
+module.exports = {
+  checkEmailLogIn,
+};
--- server/index.js
+++ server/index.js
@@ -1,29 +1,37 @@
 const express = require("express");
 
+require("./utils/mongo_db");
 
-require('./utils/mongo_db')
-
-const morgan = require('./utils/morgan');
-const error404 = require('./middlewares/error404');
+const morgan = require("./utils/morgan");
+const error404 = require("./middlewares/error404");
 
 const app = express();
 const port = 3000;
 const router = require("./routes/api-router");
-
-app.use(express.json());
-
-app.use("/api", router);
-
-const dataRoutes = require('./routes/data_routes');
+require("./utils/mongo_db");
+const morgan = require("./utils/morgan");
+const error404 = require("./middlewares/error404");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
-app.use(express.static('public'));
+app.use(morgan(":method :host :status :param[id] - :response-time ms :body"));
+app.use(express.static("public"));
 
+const dataRoutes = require("./routes/data_routes");
+const authRoutes = require("./routes/auth_routes");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan(":method :host :status :param[id] - :response-time ms :body"));
+app.use(express.static("public"));
 
 //Rutas API
-app.use('/api', dataRoutes);
+app.use("/api", dataRoutes);
+app.use("/api", router);
+app.use("/", authRoutes);
+
+// Errores
+app.use(error404);
 
 // Errores
 app.use(error404);
