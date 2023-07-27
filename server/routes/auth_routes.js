3cf5865b3fe3cef6509d616c7dee2ad5586ff675
aerozfx <aerozfx@gmail.com>
2023-07-27 22:25:05 +0200

    few changes routes

--- server/routes/auth_routes.js
+++ server/routes/auth_routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const authRouter = express.Router();
 const authController = require("../controllers/auth");
 const passport = require("passport");
-const jwt = require("jsonwebtoken");
 require("../utils/auth");
 
 //LOG IN
@@ -20,23 +19,7 @@ authRouter.get(
 authRouter.get(
   "/google/callback",
   passport.authenticate("google", { failureRedirect: "/" }),
-  (req, res) => {
-    const payload = {
-      //save here data
-      check: true,
-    };
-    const token = jwt.sign(payload, `secret_key`, {
-      expiresIn: "20m",
-    });
-    console.log(req.user);
-    res
-      .status(200)
-      .cookie("access-token", token, {
-        httpOnly: true,
-        sameSite: "lax",
-      })
-      .redirect("http://localhost:5173/home");
-  }
+  authController.googleLogin
 );
 
 // //SIGN UP
