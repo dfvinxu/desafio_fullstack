50b7de13a8ac09d25f271b11f1a392a5c9f23ec7
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-07-28 09:49:22 +0200

    icon/search bar

--- client/src/components/Main/Home/Home.jsx
+++ client/src/components/Main/Home/Home.jsx
@@ -1,20 +1,23 @@
 import { useMemo } from "react";
-import {useLoadScript} from "@react-google-maps/api"
-import Map from "./Map"
-import Navbar from "./Navbar" 
-
+import { useLoadScript } from "@react-google-maps/api";
+import Map from "./Map";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar/SearchBar";
+import Icons from "./Icons/Icons";
 
 const Home = () => {
-  const {isLoaded} = useLoadScript({
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_API
-  })
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_API,
+  });
 
-  if(!isLoaded) return <div >Loading...</div>
-  
-  return(
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return (
     <>
-      <Navbar />
+      <SearchBar />
+      <Icons />
       <Map />
+      <Navbar />
     </>
   );
 };
---
+++ client/src/components/Main/Home/SearchBar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
