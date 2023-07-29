c41cb07e9ae5bd1e7a84776f7db51d1d68e029e9
Rebeca <becabecks3.gmail.com>
2023-07-29 18:26:15 +0200

    add/LS+styles

--- client/src/components/Main/Weather/Weather.jsx
+++ client/src/components/Main/Weather/Weather.jsx
@@ -1,61 +1,81 @@
 import React, { useEffect, useState } from "react";
-import {BsFillSunFill} from 'react-icons/bs'
+import { BsFillSunFill } from "react-icons/bs";
 
 const Weather = () => {
   const [temperatureData, setTemperatureData] = useState([]);
   const [currentTemperature, setCurrentTemperature] = useState(null);
   const [nextFiveDays, setNextFiveDays] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchTemperatureData = async () => {
       const currentDate = new Date();
       const currentHour = currentDate.getHours();
-      const hours = Array.from({ length: 12 }, (_, i) => (currentHour + i * 2) % 24); // Generar horas a partir de la hora actual
+      const hours = Array.from({ length: 6 }, (_, i) => (currentHour + i * 4) % 24); // Generar horas a partir de la hora actual, dividiendo en 6 intervalos de 4 horas cada uno
       const currentYear = currentDate.getFullYear();
       const currentMonth = currentDate.getMonth() + 1;
       const currentDay = currentDate.getDate();
 
       try {
-        // 5 DIAS
-        const nextFiveDaysPromises = Array.from({ length: 5 }, (_, i) => {
-          const nextDate = new Date(currentDate);
-          nextDate.setDate(currentDate.getDate() + i + 1);
+        // Verificar si los datos están en caché
+        const cachedData = JSON.parse(localStorage.getItem("weatherData"));
 
-          const year = nextDate.getFullYear();
-          const month = nextDate.getMonth() + 1;
-          const day = nextDate.getDate();
+        if (cachedData && currentDate.getTime() - cachedData.timestamp < 3600000) {
+          // Si los datos están en caché y no han pasado más de 1 hora, usar los datos en caché
+          setTemperatureData(cachedData.temperatureData);
+          setCurrentTemperature(cachedData.currentTemperature);
+          setNextFiveDays(cachedData.nextFiveDays);
+        } else {
+          // Si los datos no están en caché o han pasado más de 1 hora, realizar las solicitudes a la API
+          const nextFiveDaysPromises = Array.from({ length: 5 }, (_, i) => {
+            const nextDate = new Date(currentDate);
+            nextDate.setDate(currentDate.getDate() + i + 1);
 
-          return fetch(
-            `https://svalencia1986.pythonanywhere.com/v1/predict?ano=${year}&mes=${month}&dia=${day}&hora=12`
-          ).then((response) => response.json());
-        });
+            const year = nextDate.getFullYear();
+            const month = nextDate.getMonth() + 1;
+            const day = nextDate.getDate();
 
-        const nextFiveDaysData = await Promise.all(nextFiveDaysPromises);
-        setNextFiveDays(nextFiveDaysData);
+            return fetch(
+              `https://svalencia1986.pythonanywhere.com/v1/predict?ano=${year}&mes=${month}&dia=${day}&hora=12`
+            ).then((response) => response.json());
+          });
 
-        // HOY
-        const currentResponse = await fetch(
-          `https://svalencia1986.pythonanywhere.com/v1/predict?ano=${currentYear}&mes=${currentMonth}&dia=${currentDay}&hora=${currentHour}`
-        );
-        const currentData = await currentResponse.json();
-        setCurrentTemperature(currentData.Prediccion_temperatura_Madrid);
+          const nextFiveDaysData = await Promise.all(nextFiveDaysPromises);
+          setNextFiveDays(nextFiveDaysData);
 
-        // PROXIMAS 12 HORAS
-        const promises = hours.map(async (hour) => {
-          const response = await fetch(
-            `https://svalencia1986.pythonanywhere.com/v1/predict?ano=${currentYear}&mes=${currentMonth}&dia=${currentDay}&hora=${hour}`
+          const currentResponse = await fetch(
+            `https://svalencia1986.pythonanywhere.com/v1/predict?ano=${currentYear}&mes=${currentMonth}&dia=${currentDay}&hora=${currentHour}`
           );
-          const data = await response.json();
-          return {
-            hour,
-            temperature: data.Prediccion_temperatura_Madrid,
-            weather_description: data.weather_description,
+          const currentData = await currentResponse.json();
+          setCurrentTemperature(currentData.Prediccion_temperatura_Madrid);
+
+          const promises = hours.map(async (hour) => {
+            const response = await fetch(
+              `https://svalencia1986.pythonanywhere.com/v1/predict?ano=${currentYear}&mes=${currentMonth}&dia=${currentDay}&hora=${hour}`
+            );
+            const data = await response.json();
+            return {
+              hour,
+              temperature: data.Prediccion_temperatura_Madrid,
+            };
+          });
+          const results = await Promise.all(promises);
+          setTemperatureData(results);
+
+          // Guardar los datos en caché
+          const dataToCache = {
+            timestamp: currentDate.getTime(),
+            temperatureData: results,
+            currentTemperature: currentData.Prediccion_temperatura_Madrid,
+            nextFiveDays: nextFiveDaysData,
           };
-        });
-        const results = await Promise.all(promises);
-        setTemperatureData(results);
+          localStorage.setItem("weatherData", JSON.stringify(dataToCache));
+        }
+
+        setIsLoading(false);
       } catch (error) {
         console.error("Error en la solicitud a la API:", error);
+        setIsLoading(false);
       }
     };
 
@@ -67,23 +87,34 @@ const Weather = () => {
 
   return (
     <section className="temp-container">
-      <h2>El tiempo en Madrid</h2>
+      <h2 className="temp-title">El tiempo en Madrid</h2>
       <section className="day-temp">
-        <p>
-          {currentDate.getDate()} de {currentDate.toLocaleString("es", { month: "long" })} {dayOfWeek}
-        </p>
+        <article className="date-container">
+          <p>{dayOfWeek}</p>
+          <p>{currentDate.getDate()} </p>
+        </article>
+        <article className="hour-container">
+          <p>{currentDate.getHours()}:{currentDate.getMinutes()}</p>
+        </article>
         {currentTemperature !== null ? (
-          <p>Temperatura actual: {currentTemperature} °C  Soleado <BsFillSunFill /> </p>
+          <article className="curr-temp">
+            <p> {currentTemperature} °C  </p>
+            <article className="curr-des">
+            <p><BsFillSunFill /></p>
+            <p>Soleado</p>
+            </article>
+            </article>
         ) : (
           <p>Cargando la temperatura actual...</p>
+        
         )}
       </section>
       <section className="hour-temp">
-        <h3>Temperaturas por horas:</h3>
+        <h3>Próximas horas</h3>
         {temperatureData.length > 0 ? (
-          <ul>
+          <ul className="hour-list">
             {temperatureData.map((data) => (
-              <li key={data.hour}>
+              <li key={data.hour} className="hour-item">
                 {data.hour}:00 - {data.temperature} °C Soleado <BsFillSunFill />
               </li>
             ))}
