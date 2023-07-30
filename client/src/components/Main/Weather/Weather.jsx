c53c548b9d07b043e64b1ac915d18bebfa83353d
Rebeca <becabecks3.gmail.com>
2023-07-30 22:06:05 +0200

    fix/night-forecast

--- client/src/components/Main/Weather/Weather.jsx
+++ client/src/components/Main/Weather/Weather.jsx
@@ -5,27 +5,25 @@ const Weather = () => {
   const [currentTemperature, setCurrentTemperature] = useState(null);
   const [nextFiveDays, setNextFiveDays] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [temperatureAt3AM, setTemperatureAt3AM] = useState([]);
 
   useEffect(() => {
     const fetchTemperatureData = async () => {
       const currentDate = new Date();
       const currentHour = currentDate.getHours();
-      const hours = Array.from({ length: 6 }, (_, i) => (currentHour + i * 4) % 24); // Generar horas a partir de la hora actual, dividiendo en 6 intervalos de 4 horas cada uno
+      const hours = Array.from({ length: 6 }, (_, i) => (currentHour + i * 4) % 24);
       const currentYear = currentDate.getFullYear();
       const currentMonth = currentDate.getMonth() + 1;
       const currentDay = currentDate.getDate();
 
       try {
-        // Verificar si los datos están en caché
         const cachedData = JSON.parse(localStorage.getItem("weatherData"));
 
         if (cachedData && currentDate.getTime() - cachedData.timestamp < 3600000) {
-          // Si los datos están en caché y no han pasado más de 1 hora, usar los datos en caché
           setTemperatureData(cachedData.temperatureData);
           setCurrentTemperature(cachedData.currentTemperature);
           setNextFiveDays(cachedData.nextFiveDays);
         } else {
-          // Si los datos no están en caché o han pasado más de 1 hora, realizar las solicitudes a la API
           const nextFiveDaysPromises = Array.from({ length: 5 }, (_, i) => {
             const nextDate = new Date(currentDate);
             nextDate.setDate(currentDate.getDate() + i + 1);
@@ -61,7 +59,6 @@ const Weather = () => {
           const results = await Promise.all(promises);
           setTemperatureData(results);
 
-          // Guardar los datos en caché
           const dataToCache = {
             timestamp: currentDate.getTime(),
             temperatureData: results,
@@ -81,8 +78,47 @@ const Weather = () => {
     fetchTemperatureData();
   }, []);
 
+  useEffect(() => {
+    const fetchTemperatureAt3AM = async () => {
+      try {
+        const temperatures = await Promise.all(
+          nextFiveDays.map(async (data, index) => {
+            const tempAt3AM = await calculateTemperatureAt3AM(data.Prediccion_temperatura_Madrid, index);
+            return tempAt3AM;
+          })
+        );
+        setTemperatureAt3AM(temperatures);
+      } catch (error) {
+        console.error("Error fetching temperature at 3 am:", error);
+      }
+    };
+
+    fetchTemperatureAt3AM();
+  }, [nextFiveDays]);
+
   const currentDate = new Date();
   const dayOfWeek = new Intl.DateTimeFormat("es", { weekday: "long" }).format(currentDate);
+
+  const calculateTemperatureAt3AM = async (temperatureAtNoon, index) => {
+    const nextDate = new Date(currentDate.getTime() + (index + 1) * 24 * 60 * 60 * 1000);
+    nextDate.setHours(3);
+
+    const year = nextDate.getFullYear();
+    const month = nextDate.getMonth() + 1;
+    const day = nextDate.getDate();
+    const hour = nextDate.getHours();
+
+    try {
+      const response = await fetch(
+        `https://svalencia1986.pythonanywhere.com/v1/predict?ano=${year}&mes=${month}&dia=${day}&hora=${hour}`
+      );
+      const data = await response.json();
+      return `${Math.floor(data.Prediccion_temperatura_Madrid)} °C`;
+    } catch (error) {
+      console.error("Error en la solicitud a la API:", error);
+      return "N/A";
+    }
+  };
 
   return (
     <section className="temp-container">
@@ -100,7 +136,7 @@ const Weather = () => {
             <p> {Math.floor(currentTemperature)} °  </p>
             <article className="curr-des">
             <p>
-              <img className="sun-icon" src="../../src/assets/sun.png"></img>
+              <img className="sun-icon" src="../../src/assets/sun.png" alt="Sun icon" />
             </p>
             <p>Soleado</p>
             </article>
@@ -115,7 +151,7 @@ const Weather = () => {
           <ul className="hour-list">
             {temperatureData.map((data) => (
               <li key={data.hour} className="hour-item">
-                {data.hour}:00 <img className="sun-icon" src="../../src/assets/sun.png"></img>  {Math.floor(data.temperature)} ° 
+                {data.hour}:00 <img className="sun-icon" src="../../src/assets/sun.png" alt="Sun icon" />  {Math.floor(data.temperature)} ° 
               </li>
             ))}
           </ul>
@@ -124,45 +160,49 @@ const Weather = () => {
         )}
       </section>
       <section className="next-temp">
-  <h3>Temperaturas los próximos 5 días</h3>
-  {nextFiveDays.length > 0 ? (
-    <table className="weather-table">
-      <thead>
-        <tr>
-          <th></th>
-          <th></th>
-          <th> <img className="sun-icon" src="../../src/assets/sun.png" alt="Sun icon" /></th>
-        </tr>
-      </thead>
-      <tbody>
-        {nextFiveDays.map((data, index) => (
-          <tr key={index}>
-            <td>
-              {new Date(currentDate.getTime() + (index + 1) * 24 * 60 * 60 * 1000).toLocaleDateString("es", {
-                day: "numeric",
-                weekday: "long"
-              })}
-            </td>
-            <td>
-              <img className="sun-icon" src="../../src/assets/sun.png" alt="Sun icon" />
-            </td>
-            <td>
-              {Math.floor(data.Prediccion_temperatura_Madrid)} °C
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  ) : (
-    <p>Cargando la predicción de temperatura para los próximos días...</p>
-  )}
-</section>
-
+        <h3>Temperaturas los próximos 5 días</h3>
+        {nextFiveDays.length > 0 ? (
+          <table className="weather-table">
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+                <th>
+                  <img className="sun-icon" src="../../src/assets/sun.png" alt="Sun icon" />
+                </th>
+                <th>
+                  <img className="luna-icon" src="../../src/assets/luna.png"></img>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {nextFiveDays.map((data, index) => (
+                <tr key={index}>
+                  <td>
+                    {new Date(currentDate.getTime() + (index + 1) * 24 * 60 * 60 * 1000).toLocaleDateString("es", {
+                      day: "numeric",
+                      weekday: "long",
+                    })}
+                  </td>
+                  <td>
+                    <img className="sun-icon" src="../../src/assets/sun.png" alt="Sun icon" />
+                  </td>
+                  <td className="days-temp">{Math.floor(data.Prediccion_temperatura_Madrid)} °C</td>
+                  <td className="days-temp">{temperatureAt3AM[index]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>Cargando la predicción de temperatura para los próximos días...</p>
+        )}
+      </section>
     </section>
   );
 };
 
 export default Weather;
+
 
 
 
