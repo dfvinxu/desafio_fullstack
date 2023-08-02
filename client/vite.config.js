07acf0c05cd3762550def7c6c34b75effc589490
aerozfx <aerozfx@gmail.com>
2023-08-02 18:27:20 +0200

    fix home styles

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: true,
+    }),
+  ],
   server: {
     proxy: {
       "/auth/signup": "http://localhost:3000",
