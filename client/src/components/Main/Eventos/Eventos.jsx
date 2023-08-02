522a52e1facde98e81b81a3305fcb2d0c8d52bf4
aerozfx <aerozfx@gmail.com>
2023-08-02 08:45:44 +0200

    remove unused dependencies

--- client/src/components/Main/Eventos/Eventos.jsx
+++ client/src/components/Main/Eventos/Eventos.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { AiOutlineHeart, AiOutlineLink } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
