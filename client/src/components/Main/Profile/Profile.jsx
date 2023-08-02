ce72d2abfef8547196f4bb46c73687656685d8a2
aerozfx <aerozfx@gmail.com>
2023-08-02 20:08:57 +0200

    update profile style

--- client/src/components/Main/Profile/Profile.jsx
+++ client/src/components/Main/Profile/Profile.jsx
@@ -1,60 +1,69 @@
-import React from "react";
-import { GrFormNext } from "react-icons/gr";
-import { AiOutlineCloseCircle } from "react-icons/ai";
-import { CgProfile } from "react-icons/cg";
-import { GrFavorite } from "react-icons/gr";
-import { TfiWorld } from "react-icons/tfi";
+import Close from "../../../../public/figma_svg/xmark.svg"
+import ProfileIcon from "../../../../public/figma_svg/profile-gray.svg"
+import ProfileSettings from "../../../../public/figma_svg/profile-settings.svg"
+import Faq from "../../../../public/figma_svg/faq.svg"
+import Messages from "../../../../public/figma_svg/mensajes.svg"
+import Language from "../../../../public/figma_svg/language.svg"
+import Forward from "../../../../public/figma_svg/go-forward.svg"
 
 const Profile = () => {
   return (
-    <article className="profile-container">
-      <AiOutlineCloseCircle className="close-icon" />
+    <section className="profile-container">
+      <Close />
       <section className="profile-header">
         <img
           className="profile-picture"
           src="https://images3.memedroid.com/images/UPLOADED158/63ae8a9794f6e.jpeg"
-          alt="Profile"
+          alt="profile picture"
         />
-        <h1>
-          Moyai <br />
-          Moyai@gmail.com
-        </h1>
+        <article className="profile-info">
+          <p className="profile-name">Andrea</p>
+          <p className="profile-email">andrea@gmail.com</p>
+        </article>
       </section>
-      <section className="profile-sections">
-        <div className="section">
-          <h2>
-            <CgProfile /> Mi perfil <GrFormNext className="next-icon" />
-          </h2>
-        </div>
-        <div className="section">
-          <h2>
-            <GrFavorite /> Tu Actividad <GrFormNext className="next-icon" />
-          </h2>
-        </div>
-        <div className="section">
-          <h2>
-            <TfiWorld /> Español <GrFormNext className="next-icon" />
-          </h2>
-        </div>
+      <section className="profile-options">
+        <article className="option">
+          <span className="option-section">
+            <ProfileIcon /> Mi perfil 
+          </span>
+          <Forward className="next-icon" />
+        </article>
+        <article className="option">
+          <span className="option-section">
+            <ProfileSettings /> Ajustes 
+          </span>
+          <Forward className="next-icon" />
+        </article>
+        <article className="option">
+        <span className="option-section">
+            <Faq /> FAQs 
+          </span> <Forward className="next-icon" />
+        </article>
+        <article className="option">
+        <span className="option-section">
+            <Messages /> Mensajes 
+          </span> <Forward className="next-icon" />
+        </article>
       </section>
-      <div>
-        <button className="logout-btn">Cerrar sesión</button>
-      </div>
+      <section className="profile-language">
+        <span className="option-section">
+          <Language /> Español 
+        </span> <Forward className="next-icon" />
+      </section>
+      <button className="logout-btn">Cerrar sesión</button>
       {/* Footer */}
       <footer className="profile-footer">
-        <div className="left-content">
-          <img
+        <p>Política y privacidad</p>
+        <article className="collab">
+            <img
             className="community-flag"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_the_Community_of_Madrid.svg/800px-Flag_of_the_Community_of_Madrid.svg.png"
             alt="Community of Madrid Flag"
           />
-          <p>Colaboración especial con la Comunidad de Madrid</p>
-        </div>
-        <div className="center-content">
-          <p>Política y privacidad</p>
-        </div>
+            <p>Colaboración especial con la Comunidad de Madrid</p>
+          </article>
       </footer>
-    </article>
+    </section>
   );
 };
 
