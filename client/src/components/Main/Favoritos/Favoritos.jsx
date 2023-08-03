ccfc304ced1b2a4454d38d73821eb84ca895396c
Rebeca <becabecks3.gmail.com>
2023-08-03 16:47:38 +0200

    fix/heart

--- client/src/components/Main/Favoritos/Favoritos.jsx
+++ client/src/components/Main/Favoritos/Favoritos.jsx
@@ -2,10 +2,17 @@ import { useState, useEffect, useContext } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import jwt_decode from "jwt-decode"
 import { AuthContext } from '../../../context/authContext';
+import { AiOutlineHeart } from "react-icons/ai";
+import { AiOutlineLink } from "react-icons/ai";
+import { AiFillHeart } from 'react-icons/ai'
+import { BsSearch } from "react-icons/bs";
+import BackButton from "../BackButton";
+
 
 const Favoritos = () => {
   const {authCookie} = useContext(AuthContext)
   const [favoriteEvents, setFavoriteEvents] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchFavoriteEvents = async () => {
@@ -30,23 +37,58 @@ const Favoritos = () => {
     fetchFavoriteEvents();
   }, [authCookie]);
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <section>
+    <section className="favorites">
+      <article className="search-bar-container">
+        <BackButton link={"/home"} />
+        <article className="search-bar">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder="Buscar..."
+          />
+          <BsSearch className="icon" />
+        </article>
+      </article>
       <h2>Tus eventos favoritos</h2>
-      <ul>
+      <section className="favorites-frame">
         {favoriteEvents.length > 0 ? (
           favoriteEvents.map((event) => (
-            <li key={uuidv4()}>
-              <h3>{event.TITULO}</h3>
-              <p>Dirección: {event.DIRECCION}</p>
-              <p>Fecha: {event.FECHA}</p>
-              <p>Hora: {event.HORA}</p>
-            </li>
+            <article key={uuidv4()} className="event-card">
+              <img 
+                src="https://cdn.siasat.com/wp-content/uploads/2019/11/events-in-hyderabad.jpg"
+                alt="event"
+                className="event-image"
+              />
+              <section className="event-info">
+                <article className="event-details">
+                <p className="event-date">{event.FECHA}</p>
+                <h3 className="event-title">{event.TITULO}</h3>
+                <p className="event-address">{event.DIRECCION}</p>
+                </article>
+              
+                <section className="event-icons">
+                    <AiOutlineHeart className="event-icon" />
+                    <a
+                      href={`${event["CONTENT-URL"]}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <AiOutlineLink />
+                    </a>
+                </section>
+              </section>
+            </article> 
           ))
         ) : (
           <p>No tienes eventos favoritos.</p>
         )}
-      </ul>
+      </section>
     </section>
   );
 } 
